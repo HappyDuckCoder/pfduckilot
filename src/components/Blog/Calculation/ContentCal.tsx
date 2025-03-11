eff7c7c1ad981c0893074875f61424224505e261
duckilot <prto2802@gmail.com>
2025-03-12 05:28:05 +0700

    dang lam khoi tao tre

--- src/components/Blog/Calculation/ContentCal.tsx
+++ src/components/Blog/Calculation/ContentCal.tsx
@@ -2,20 +2,19 @@ import React from "react";
 
 const ContentCal = ({
   title,
-  descriptionL1,
-  descriptionL2,
+  listDescription,
 }: {
   title: string;
-  descriptionL1: string;
-  descriptionL2: string;
+  listDescription: string[];
 }) => {
   return (
     <>
       <h4 className="font-semibold mb-3">{title}</h4>
       <p className="mb-4">
         <ol className="list-disc list-outside">
-          <li className="bf-bullet">{descriptionL1}</li>
-          <li className="bf-bullet">{descriptionL2}</li>
+          {listDescription.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
         </ol>
       </p>
     </>
